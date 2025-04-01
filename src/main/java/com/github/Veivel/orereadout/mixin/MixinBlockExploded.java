885e985c09bbc5d42fee3f74c01fde6f3e3fe1cf
Veivel <25278964+Veivel@users.noreply.github.com>
2025-04-01 15:54:51 +0700

    feat: initial impl of logging user-caused explosions

---
+++ src/main/java/com/github/Veivel/orereadout/mixin/MixinBlockExploded.java
@@ -0,0 +1,55 @@
+package com.github.Veivel.orereadout.mixin;
+
+import java.util.Map;
+import java.util.function.BiConsumer;
+
+import org.apache.logging.log4j.Logger;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.github.Veivel.config.ConfigManager;
+import com.github.Veivel.config.ModConfig;
+import com.github.Veivel.notifier.Notifier;
+import com.github.Veivel.orereadout.OreReadoutMod;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.explosion.Explosion;
+
+@Mixin(AbstractBlock.class)
+public class MixinBlockExploded {
+  private static final Logger LOGGER = OreReadoutMod.LOGGER;
+  private static ModConfig config = ConfigManager.getConfig();
+
+  @Inject(
+    method = "onExploded(Lnet/minecraft/block/BlockState;Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/world/explosion/Explosion;Ljava/util/function/BiConsumer;)V", 
+    at = @At("HEAD")
+  )
+  public void onExploded(BlockState state, ServerWorld world, BlockPos pos, Explosion explosion, BiConsumer<ItemStack,BlockPos> stackMerger, CallbackInfo ci) {
+    Block block = state.getBlock();
+    Map<String, Boolean> map = config.getBlockMap();
+    String mapKeySet = map.keySet().toString();
+    String blockName = Registries.BLOCK.getId(block).toString().replaceFirst("minecraft:", "");
+    
+    LOGGER.debug("Checking if block {} is in map of {}.", blockName, mapKeySet);
+    
+    if (map.containsKey(blockName)) {
+      LivingEntity entity = explosion.getCausingEntity();
+      if (entity.isPlayer()) {
+        LOGGER.debug("Sending notification!");
+        Notifier.notify(blockName, pos, world, (PlayerEntity) entity);
+      }
+    }
+  }
+}
